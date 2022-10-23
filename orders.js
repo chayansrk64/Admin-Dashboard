42c1316c3377dd997e3c44ff4db83e3d138f991b
chayansrk64 <chayansrk64@gmail.com>
2022-10-23 13:19:41 +0600

    first commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        ProductNumber: '85456',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Larvender kd343 Drone',
+        ProductNumber: '85456',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'DSLR CAMERA',
+        ProductNumber: '85456',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        ProductNumber: '85456',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        ProductNumber: '85456',
+        paymentStatus: 'Paid',
+        shipping: 'Delevered'
+    },
+]
